a77aa4572a79be6760e06d90d74d7740dc9846fe
tukaram <tukaram@2796735d-4f1f-0410-83a5-ff15bd95aad1>
2006-12-27 22:11:39 +0000

    Comments added

--- trunk/eclipseWork/takanuka/openaqua/defaultCommands/CmdUsageCounter.java
+++ trunk/eclipseWork/takanuka/openaqua/defaultCommands/CmdUsageCounter.java
@@ -9,6 +9,8 @@ import openaqua.base.ICommand;
 import openaqua.base.IRecord;
 
 /**
+ * 
+ * 
  * @author tukaram
  *
  */
@@ -26,10 +28,21 @@ public class CmdUsageCounter implements ICommand{
 		return true;
 	}
 	
+	
+	/**
+	 * Return the collected amount of usages of this command
+	 * 
+	 * @return amount
+	 */
 	public int returnCounter() {
 		return usage;
 	}
 
+	
+	/**
+	 * Set the counter to 0
+	 *
+	 */
 	public void resetCounter() {
 		lock.writeLock().lock();
 		try {
