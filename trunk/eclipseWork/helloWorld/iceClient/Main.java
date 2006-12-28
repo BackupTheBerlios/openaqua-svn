568b13f9e0c5eeb3df35b991ae676303f7425ee6
tukaram <tukaram@2796735d-4f1f-0410-83a5-ff15bd95aad1>
2006-12-28 17:56:45 +0000

    ice Tests added

---
+++ trunk/eclipseWork/helloWorld/iceClient/Main.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package iceClient;
+
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+/**
+ * @author tukaram
+ *
+ */
+public class Main {
+	private static Logger logger = Logger.getRootLogger();
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			//Setup a Simple Logger
+			PatternLayout layout = new PatternLayout( "%-5p\t[%t] %c: %m%n" );
+			
+			//Logger for Console
+	        ConsoleAppender consoleAppender = new ConsoleAppender( layout );
+	        logger.addAppender( consoleAppender );
+	        
+	        //Logger for File
+	        //FileAppender fileAppender = new FileAppender( layout, "MeineLogDatei.log", false );
+	        //logger.addAppender( fileAppender );
+	        
+	        //Set Default Debug Level
+	        // ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF:
+	        logger.setLevel( Level.ALL);
+
+	        //setup and execute the Connector object
+	        logger.info("Done");
+	        
+		} catch( Exception ex ) {
+			ex.printStackTrace();
+			System.out.println( ex );
+	    }
+
+	}
+
+}
