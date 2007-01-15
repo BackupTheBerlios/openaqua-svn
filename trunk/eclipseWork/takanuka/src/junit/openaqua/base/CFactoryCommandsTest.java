4722525dd9f756c6d072e40675e9a01c4585bbd4
cbehrens <cbehrens@2796735d-4f1f-0410-83a5-ff15bd95aad1>
2007-01-15 14:55:48 +0000

    moving dirs

---
+++ trunk/eclipseWork/takanuka/src/junit/openaqua/base/CFactoryCommandsTest.java
@@ -0,0 +1,109 @@
+package openaqua.base;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+
+public class CFactoryCommandsTest extends TestCase {
+
+	@Test
+	public final void testGetInstance() {
+		CFactoryCommands a = CFactoryCommands.getInstance();
+		CFactoryCommands b = CFactoryCommands.getInstance();
+		assertTrue(a == b);    	
+		assertTrue(a != null);
+	}
+
+	@Test
+	public final void testRegisterBuilder() {
+		try {
+			CFactoryCommands.getInstance().registerBuilder(null);
+			fail("Should throw NullPointerException");
+		} catch( NullPointerException e) {
+			// its ok
+		}
+
+
+	}
+
+	@Test
+	public final void testUnregisterBuilder() {
+		;
+	}
+
+	@Test
+	public final void testGetCommand() {
+		;
+	}
+
+	@Test
+	public final void testCreateCommand() {
+		;
+	}
+
+	@Test
+	public final void testAddCommand() {
+		;
+	}
+
+	@Test
+	public final void testObject() {
+		;
+	}
+
+	@Test
+	public final void testGetClass() {
+		;
+	}
+
+	@Test
+	public final void testHashCode() {
+		;
+	}
+
+	@Test
+	public final void testEquals() {
+		;
+	}
+
+	@Test
+	public final void testClone() {
+		;
+	}
+
+	@Test
+	public final void testToString() {
+		;
+	}
+
+	@Test
+	public final void testNotify() {
+		;
+	}
+
+	@Test
+	public final void testNotifyAll() {
+		;
+	}
+
+	@Test
+	public final void testWaitLong() {
+		;
+	}
+
+	@Test
+	public final void testWaitLongInt() {
+		;
+	}
+
+	@Test
+	public final void testWait() {
+		;
+	}
+
+	@Test
+	public final void testFinalize() {
+		;
+	}
+
+}
---
+++ trunk/eclipseWork/takanuka/src/junit/openaqua/defaultCommands/AllTests.java
@@ -0,0 +1,18 @@
+package openaqua.defaultCommands;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for openaqua.defaultCommands");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(CmdDoNothingTest.class);
+		suite.addTestSuite(CmdUsageCounterTest.class);
+		suite.addTestSuite(CmdDumpRecordTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
